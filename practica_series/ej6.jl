da164d5453eb53d7e763cd2ccd1c41b46b11b3c2
valuvilla <valuvp@hotmail.com>
2023-03-07 11:55:26 +0100

    cambios

--- practica_series/ej6.jl
+++ practica_series/ej6.jl
@@ -5,3 +5,13 @@ a(n) = (-1)^n*1/(2^(2n)*factorial(n)^2)
 c(n) = (-1)^n/(2^(2n)*factorial(n)^2)
 limit(abs(c(n)/c(n+1)), n => Inf)
 
+
+#Suma funcional parcial de grado 1: x
+using Plots, LaTeXStrings, Bessels
+@vars x real=true
+a(x,n) = (-1)^n/(2^(2n)*factorial(n)^2) * x^(2n)
+N = 10
+an = [a(x,n) for n=0:N]
+An = sum(an)
+plot(An, -8, 8, label="Suma funcional parcial", legend=:bottomright)
+plot!(besselj0)
---
+++ practica_series/ej7.jl
@@ -0,0 +1,14 @@
+#series de Fourier
+
+using Plots, SymPy, Latexify
+@vars t real=true
+a(t,n) = sin(n*t)
+N = 100
+an = [a(t,n) for n=1:N]
+An = cumsum(an)
+plot(An[N], -10, 10)
+plots = []  # Array para guardar las gráficas
+for i in An
+    push!(plots, plot(i, label=latexify(i), legend=:outertop))
+end
+plot(plots..., layout=(5,2), size=(800,1600))
