1ee36fdd3cb12a5fe866122359b3c8282d28f2c8
valuvilla <valuvp@hotmail.com>
2023-05-16 12:16:35 +0200

    g

--- practicas_integrales/ejercicio13.jl
+++ practicas_integrales/ejercicio13.jl
@@ -7,3 +7,13 @@ V(x) = integrate(PI*f⁻¹(y)^2, 0, x)
 V(x)
 # Nivel para un volumen de 100 m³
 nivel = solve(V(x)-100)[2]
+
+
+δ = 900
+const g = 9.81
+
+# fuerza
+PI*f⁻¹(y)^2*δ*g # m * (a=g)
+
+# trabajo
+N(integrate(PI*f⁻¹(y)^2*δ*g*(8-y), 0, nivel))
