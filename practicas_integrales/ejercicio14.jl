07cec2c73d74c4269bea02e33640e9b6f9f0898a
valuvilla <valuvp@hotmail.com>
2023-05-16 12:23:12 +0200

    g

--- practicas_integrales/ejercicio14.jl
+++ practicas_integrales/ejercicio14.jl
@@ -1,23 +1,23 @@
 using SymPy, Plots
 @vars x real = true
 f(x) = x-1
-g(x) = (x-1)^2
+h(x) = (x-1)^2
 # Calculo de los puntos de corte.
-a, b = N(solve(f(x)-g(x)))
+a, b = N(solve(f(x)-h(x)))
 plot(f(x), a, b, aspect_ratio = 1, label = "")
-plot!(g(x), a, b, fillrange = f, fillalpha = 0.3, aspect_ratio = 1, c = 1, label = "")
+plot!(h(x), a, b, fillrange = f, fillalpha = 0.3, aspect_ratio = 1, c = 1, label = "")
 
 
 
 # Coordenada x del centroide.
-cx = integrate(x*(f(x)-g(x)), a, b) / integrate(f(x)-g(x), a, b)
+cx = integrate(x*(f(x)-h(x)), a, b) / integrate(f(x)-h(x), a, b)
 # Coordenada y del centroide
-cy = 1/2 * integrate(f(x)^2-g(x)^2, a, b) / integrate(f(x)-g(x), a, b)
+cy = 1/2 * integrate(f(x)^2-h(x)^2, a, b) / integrate(f(x)-h(x), a, b)
 # Dibujamos el centroide
 scatter!((cx,cy), label = "centroide")
 
 
-2*PI*cy*integrate(f(x)-g(x), a, b)
+2*PI*cy*integrate(f(x)-h(x), a, b)
 
 
-2*PI*cx*integrate(f(x)-g(x), a, b)
+2*PI*cx*integrate(f(x)-h(x), a, b)
